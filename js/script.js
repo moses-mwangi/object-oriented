edea4cd89fac36c537b2b0d8ce53569baf35de3b
moses <moses.mwangi.me@gmail.com>
2024-01-11 13:23:06 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 };
 const moses = new Person("Moses", 2002);
-const mwangi = new Person("Moses", 2004);
-console.log(moses);
+const mwangi = new Person("john", 2004);
+console.log(moses, mwangi);
 
 //////////////////prototype
 console.log(Person.prototype);
@@ -15,3 +15,26 @@ Person.prototype.calcage = function () {
 };
 moses.calcage();
 mwangi.calcage();
+
+console.log(moses.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(moses));
+console.log(moses.hasOwnProperty("name"));
+Person.prototype.species = "Homo sapien";
+console.log(moses.hasOwnProperty("firstName"));
+console.log(moses.hasOwnProperty("name"));
+
+console.log(moses.__proto__);
+
+//////object prototype(top of prototype)
+console.log(moses.__proto__.__proto__);
+console.log(moses.__proto__.__proto__.__proto__);
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 5, 6, 3, 2, 8, 7, 9, 7];
+console.log(arr.__proto__);
+console.log(arr.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
